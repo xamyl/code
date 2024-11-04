d5a926ff263fdeeca351837fefb3b03cecd7d34b
xam <awesomematylda@gmail.com>
2024-11-04 16:08:08 -0500

    Delete packages/app-lib directory
    Signed-off-by: xam <awesomematylda@gmail.com>

--- packages/app-lib/src/config.rs
+++
@@ -1,6 +0,0 @@
-//! Configuration structs
-
-pub const MODRINTH_API_URL: &str = "https://staging-api.modrinth.com/v2/";
-pub const MODRINTH_API_URL_V3: &str = "https://staging-api.modrinth.com/v3/";
-
-pub const META_URL: &str = "https://launcher-meta.modrinth.com/";
